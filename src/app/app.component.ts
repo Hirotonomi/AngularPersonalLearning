2671bed742cf586559aee30306e5d0304a6a0216
Hirotonomi <Asztonpsiak@gmail.com>
2024-03-19 15:50:08 +0100

    Pushing idk where

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { BidingUserComponent } from './biding-user/biding-user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet,HeaderComponent,BidingUserComponent],
+
+  
+  template: `<app-header></app-header>
+  <app-biding-user [bindVar]="bindVar"></app-biding-user>`,
+
+
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'project';
+  bindVar = "ugabuga"
 }
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  template: `
+  <div class="dupa">
+    <img src="https://i.pinimg.com/474x/b4/26/ae/b426ae05e7a511c7dbfa4ee8260f7b29--fantasy-rpg-warhammer-fantasy.jpg">
+    <h1>Jan Rękas</h1>
+  
+  </div>
+`
+    ,
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+}
